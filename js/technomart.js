069cea42501166fb86585cb93bca484f2fc762f4
tveritina <tveritinanastya@gmail.com>
2020-03-01 19:04:01 +0500

    Всплывающие окна на index

--- js/technomart.js
+++ js/technomart.js
@@ -1,11 +1,36 @@
 var mapBig = document.querySelector("#map-big");
+var feedbackModal = document.querySelector("#feedback");
+var registrationModal = document.querySelector("#registration");
+
+var mapClose = document.querySelector(".map-big .modal-close");
+var feedbackClose = document.querySelector(".feedback .modal-close");
+var registrationClose = document.querySelector(".universal-modal .modal-close");
+
 var mapImage = document.querySelector(".company-info .map");
-var close = document.querySelector(".modal-close");
+var feedbackButton = document.querySelector(".company-info-button-feedback");
+var registrationButton = document.querySelector(".register-back");
 
 mapImage.addEventListener("click", function () {
-    mapBig.classList.remove('visually-hidden');
+    mapBig.classList.remove("visually-hidden");
 });
 
-close.addEventListener("click", function () {
-    mapBig.classList.add('visually-hidden');
+feedbackButton.addEventListener("click", function () {
+    feedbackModal.classList.remove("visually-hidden");
+});
+
+registrationButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    registrationModal.classList.remove("visually-hidden");
+});
+
+mapClose.addEventListener("click", function () {    
+    mapBig.classList.add("visually-hidden");
+});
+
+feedbackClose.addEventListener("click", function () {    
+    feedbackModal.classList.add("visually-hidden");
+});
+
+registrationClose.addEventListener("click", function () {    
+    registrationModal.classList.add("visually-hidden");
 });
