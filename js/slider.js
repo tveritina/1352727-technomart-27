bff3f86d83f6d71c51c4831d55fa374b2bd7979f
tveritina <tveritinanastya@gmail.com>
2020-03-01 20:18:10 +0500

    В слайдере 2 добавила изменения в меню

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,5 @@
 var firstImg = document.querySelector(".slider-list-one");
 var secondtImg = document.querySelector(".slider-list-two");
-
 var iconArrow = document.querySelectorAll(".slider-icon");
 
 iconArrow.forEach(icon => {
@@ -12,3 +11,29 @@ iconArrow.forEach(icon => {
 });
 
 
+
+var deliveryItem = document.querySelector(".delivery-service");
+var guaranteeItem = document.querySelector(".guarantee-service");
+var creditItem = document.querySelector(".credit-service");
+
+var deliveryMenu = document.querySelector(".service-menu li:nth-child(1)");
+var guaranteeMenu = document.querySelector(".service-menu li:nth-child(2)");
+var creditMenu = document.querySelector(".service-menu li:nth-child(3)");
+
+deliveryMenu.addEventListener("click", function () {
+    deliveryMenu.classList.add("service-item-active");
+    guaranteeMenu.classList.remove("service-item-active");
+    creditMenu.classList.remove("service-item-active");
+});
+
+guaranteeMenu.addEventListener("click", function () {
+    guaranteeMenu.classList.add("service-item-active");
+    deliveryMenu.classList.remove("service-item-active");
+    creditMenu.classList.remove("service-item-active");
+});
+
+creditMenu.addEventListener("click", function () {
+    creditMenu.classList.add("service-item-active");
+    deliveryMenu.classList.remove("service-item-active");
+    guaranteeMenu.classList.remove("service-item-active");
+});
